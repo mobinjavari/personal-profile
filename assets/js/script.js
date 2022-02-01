00e2397d267e5784dc70439fd25db51e4cfcdb6d
Javari <javari@duck.com>
2022-02-01 22:53:22 +0330

    create social page [app,discord,telegram,github,...]

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function OpenLink(url,protocol)
+{
+    switch (protocol) {
+        case "page":
+            location.href = url;
+            break;
+
+        case "http":
+            location.href = 'http://'+url;
+            break;
+    
+        default:
+            location.href = 'https://'+url;
+            break;
+    }
+}
